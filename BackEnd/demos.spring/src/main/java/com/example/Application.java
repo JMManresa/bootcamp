7c7f1ced466f1ca629a36a34d6e3ffc3afb39d92
JMManresa <jose-manuel.manresa-guirao@capgemini.com>
2022-02-28 17:09:18 +0100

    Language con DTO's y movidas varias

--- BackEnd/demos.spring/src/main/java/com/example/Application.java
+++ BackEnd/demos.spring/src/main/java/com/example/Application.java
@@ -1,11 +1,16 @@
 package com.example;
 
+import java.sql.Timestamp;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.domains.contracts.ActorRepository;
+import com.example.domains.contracts.repositories.LanguageRepository;
+import com.example.domains.entities.Actor;
 import com.example.ioc.Servicio;
 
 @SpringBootApplication
@@ -22,14 +27,32 @@ public class Application implements CommandLineRunner {
 	@Autowired(required = false)
 	@Qualifier("manual")
 	Servicio manual;
+	
+	@Autowired
+	ActorRepository dao;
+	
+	@Autowired
+	LanguageRepository daoLanguage;
 
 	@Override
 	public void run(String... args) throws Exception {
-		srv.run();
-		if(manual != null) manual.run();
+//		srv.run();
+//		if(manual != null) manual.run();
+//		
+//		srv.setName("coña");
+//		srv.run();
 		
-		srv.setName("coña");
-		srv.run();
+//		var a = new Actor(206, "Grillo", "Pepito", new Timestamp(0));
+		
+//		dao.findAll().forEach(System.out::println);
+//		System.err.println(dao.findById(1).get());
+//		System.err.println(dao.findByFirstName("nick"));
+//		System.err.println(dao.findByFirstNameStartingWithAndLastNameEndingWith("n", "g"));
+//		
+//		var b = dao.findById(1).get();
+//		b.getFilmActors().forEach(item -> System.out.println(item.getFilm().getTitle()));
+
+		
 	}
 
 }
---
+++ BackEnd/demos.spring/src/main/java/com/example/domains/contracts/ActorRepository.java
@@ -0,0 +1,12 @@
+package com.example.domains.contracts;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.domains.entities.Actor;
+
+public interface ActorRepository extends JpaRepository<Actor, Integer> {
+	List<Actor> findByFirstName(String name);
+	List<Actor> findByFirstNameStartingWithAndLastNameEndingWith(String prefijo, String sufijo);
+}
