81e151c7b9855c53c9b3e73c912a49eff2afab70
JMManresa <jose-manuel.manresa-guirao@capgemini.com>
2022-03-23 21:10:38 +0100

    Calculadora Angular

--- angular/src/app/calculadora/calculadora.component.ts
+++ angular/src/app/calculadora/calculadora.component.ts
@@ -7,7 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  public principal: string = "0";
+  public secundaria: string = "";
+  public acumulado: number = 0;
+  public operando: number = 0;
+  public operadorPendiente: string = "+";
+
+  calcula(nuevoOperador: string) {
+
+    switch (this.operadorPendiente) {
+      case "+":
+        this.acumulado += this.operando;
+        break;
+      case "-":
+        this.acumulado -= this.operando;
+        break;
+      case "*":
+        this.acumulado *= this.operando;
+        break;
+      case "/":
+        this.acumulado /= this.operando;
+        break;
+      case "=":
+        break;
+    }
+    this.imprimirEnSecundaria(nuevoOperador);
+    if(this.principal != this.acumulado.toString()) {
+        this.limpiar();
+        this.imprimirEnPrincipal(this.acumulado.toString());
+    }
+    this.operadorPendiente = nuevoOperador;
+    this.operando = 0;
+  }
+
+
+
+  imprimirEnPrincipal(value: string) {
+    if (this.principal == "0" || this.principal  == this.acumulado.toString()) {
+      this.principal = value;
+    } else {
+      this.principal += value;
+    }
+    //Guardamos el numero en formato string
+    this.operando += parseFloat(value);
+  }
+
+  imprimirEnSecundaria(operador: string) {
+    this.secundaria = this.acumulado + " " + operador;
+  }
+
+  limpiar() {
+    this.principal = "0";
+  }
+
+  resetear() {
+    this.principal = "0";
+    this.secundaria = "";
+    this.acumulado = 0;
+    this.operando = 0;
+    this.operadorPendiente = "+";
+  }
 
   ngOnInit(): void {
   }
